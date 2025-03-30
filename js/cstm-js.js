042fd204da133a7758df4e6bc0ad9a38c4759786
malikuzma038 <malikuzma038@gmail.com>
2025-03-30 23:58:47 +0530

    removed unused css

--- js/cstm-js.js
+++ js/cstm-js.js
@@ -1,8 +1,3 @@
-// aos animation
-AOS.init({
-	duration: 1200,
-})
-
 // Kursor js
 const cursor = new kursor({
 	type: 2,
@@ -10,6 +5,7 @@ const cursor = new kursor({
 });
 
 const hoverTargets = document.querySelectorAll('.hover-target');
+const hoverTarget3d = document.querySelectorAll('.roof');
 
 hoverTargets.forEach(target => {
 	target.addEventListener('mouseenter', () => {
@@ -17,10 +13,46 @@ hoverTargets.forEach(target => {
 		cursorElement.style.transform = 'scale(3)';
 		cursorElement.style.opacity = '0.3';
 	});
+	
 
 	target.addEventListener('mouseleave', () => {
 		const cursorElement = document.querySelector('.kursor');
 		cursorElement.style.transform = 'scale(1)';
 		cursorElement.style.opacity = '1';
 	});
+});
+
+hoverTarget3d.forEach(target => {
+	target.addEventListener('mouseenter', () => {
+		const cursorElement = document.querySelector('.kursor');
+		cursorElement.style.transform = 'scale(0.1)';
+		cursorElement.style.opacity = '0.3';
+	});
+	
+
+	target.addEventListener('mouseleave', () => {
+		const cursorElement = document.querySelector('.kursor');
+		cursorElement.style.transform = 'scale(1)';
+		cursorElement.style.opacity = '1';
+	});
+});
+
+// Tilt js
+
+$(".roof").tilt({
+	glare: true,
+	maxGlare: 0.5,
+	speed: 1500,
+	maxTilt: 15,
+	scale: 1.05,
+	perspective: 800
+});
+
+$(".hdg-roof-img").tilt({
+	glare: true,
+	maxGlare: 0.5,
+	speed: 1500,
+	maxTilt: 15,
+	scale: 1.1,
+	perspective: 800
 });
