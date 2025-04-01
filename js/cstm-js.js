e4a768bee10c646a3109ef74c3a0e9da9a9eacd2
malikuzma038 <malikuzma038@gmail.com>
2025-04-01 13:01:01 +0530

    completed file push

--- js/cstm-js.js
+++ js/cstm-js.js
@@ -1,3 +1,24 @@
+// Tilt js
+
+$(".roof, .benefit-img-1, .about-img-1, .about-img-2, .why-tab-ctnt, .blog-card, .area-check").tilt({
+	glare: true,
+	maxGlare: 0.5,
+	speed: 1500,
+	maxTilt: 15,
+	scale: 1.05,
+	perspective: 800
+});
+
+$(".hdg-roof-img").tilt({
+	glare: true,
+	maxGlare: 0.5,
+	speed: 1500,
+	maxTilt: 15,
+	scale: 1.1,
+	perspective: 800
+});
+
+
 // Kursor js
 const cursor = new kursor({
 	type: 2,
@@ -35,24 +56,4 @@ hoverTarget3d.forEach(target => {
 		cursorElement.style.transform = 'scale(1)';
 		cursorElement.style.opacity = '1';
 	});
-});
-
-// Tilt js
-
-$(".roof").tilt({
-	glare: true,
-	maxGlare: 0.5,
-	speed: 1500,
-	maxTilt: 15,
-	scale: 1.05,
-	perspective: 800
-});
-
-$(".hdg-roof-img").tilt({
-	glare: true,
-	maxGlare: 0.5,
-	speed: 1500,
-	maxTilt: 15,
-	scale: 1.1,
-	perspective: 800
 });
